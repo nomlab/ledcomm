fb57ea3a3f9033c6efbf23e8e646482c136db543
Yoshinari Nomura <nom@nomlab.org>
2021-11-01 17:24:10 +0900

    Cleanup to restart the project

--- src/recieve_str.c
+++ src/recieve_str.c
@@ -1,134 +1,74 @@
+#include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
-#include <stdio.h>
 
-#define SS_PORT 8
-#define SPI_CHANNEL 0                   //SPIチャンネル
-#define MILLI_SEC 1000000               //ms
-#define TIME 2
-#define THRESHOLD 320                   //LED brightness threshold
-//#define STR_LEN 5
+#define SHORT_DELAY 2
+#define SPI_CHANNEL 0   // SPIチャンネル
+#define SS_PORT     8
+#define THRESHOLD   320 // LED brightness threshold
 
-struct str{
-  unsigned int a0 :1;
-  unsigned int a1 :1;
-  unsigned int a2 :1;
-  unsigned int a3 :1;
-  unsigned int a4 :1;
-  unsigned int a5 :1;
-  unsigned int a6 :1;
-  unsigned int a7 :1;
-};
-
-union chr{
-  char c;
-  struct str a;
-};
-
-void btoc(union chr c, int data[8]){
-  c.a.a7 = data[7];
-  c.a.a6 = data[6];
-  c.a.a5 = data[5];
-  c.a.a4 = data[4];
-  c.a.a3 = data[3];
-  c.a.a2 = data[2];
-  c.a.a1 = data[1];
-  c.a.a0 = data[0];
-}
-
-int check_v(unsigned char v[2]){
-  if( (v[0]*256 + v[1]) >= THRESHOLD ){
-    return 1;
-  }
-  else return 0;
-}
-
-int main(void)
+int get_light_intensity()
 {
-  int i,j,k,n = 0;
-  int speed = 10000000;                         //通信速度(100Hz)
-  unsigned char spi_buff[2];                   //送受信用バッファ
-  int data[8];
-  int num;
-  char str[1024];
-  union chr c;
-  char chara;
-  int str_len =0;
-  int flag = 0;
-  int try_num = 1000;
-  //バッファ初期化
+  unsigned char spi_buf[2];
+  spi_buf[0] = 0b01100000;
 
-  //SPIチャンネル初期化
-  if((wiringPiSPISetup (SPI_CHANNEL, speed)) < 0){
-    printf("wiringPiSPISetup error \n");
+  digitalWrite(SS_PORT, 0);
+  wiringPiSPIDataRW(SPI_CHANNEL, spi_buf, 2);
+  digitalWrite(SS_PORT, 1);
+
+  return spi_buf[0] * 256 + spi_buf[1];
+}
+
+int init_spi_channel()
+{
+  int speed = 10000000; // 通信速度(100Hz)
+  if (wiringPiSPISetup(SPI_CHANNEL, speed) < 0) {
     return -1 ;
   }
-
-  printf("Setup SPI...\n");
-
-  //GPIO初期化
-  if(wiringPiSetupGpio() == -1){
-    printf("wiringPiSetupGpio error\n");
+  if (wiringPiSetupGpio() == -1) {
     return -1;
   }
-  pinMode(SS_PORT, OUTPUT);                       //22pinを出力に設定
-  digitalWrite(SS_PORT, 1);                       //SS信号初期化
-
-  printf("Start SPI...\n");
-  while(n < try_num && flag == 0){
-
-    //SPI通信実行
-    digitalWrite(SS_PORT, 0);                   //SS信号をLOW出力にして通信開始
-    spi_buff[0] = 0b01100000;
-    wiringPiSPIDataRW(SPI_CHANNEL, spi_buff, 2);             //データ送受信
-    digitalWrite(SS_PORT, 1);                   //SS信号をHIGH出力にして通信終了
-
-    //インターバル
-    printf("%d\n", spi_buff[0]*256+spi_buff[1]);                 //受信データを出力
-
-    if(check_v(spi_buff)){
-      printf("Data input.\n");
-      delay(TIME);
-      flag = 1;
-      for(i=0; i<8; i++){
-        num=0;
-        for(j=0; j<3; j++){
-          digitalWrite(SS_PORT,0);
-          spi_buff[0] = 0b01100000;
-          wiringPiSPIDataRW(SPI_CHANNEL, spi_buff,2);
-          digitalWrite(SS_PORT,1);
-          spi_buff[0] = spi_buff[0] << 6;
-          spi_buff[0] = spi_buff[0] >> 6;
-          spi_buff[1] &= 0b11111111;
-          //printf("\t%d\n", spi_buff[0]*256+spi_buff[1]);     //受信データを出力
-          if(check_v(spi_buff))num += 1;
-          delay(TIME);
-        }
-        if (num >= 2) num= 1;
-        else {
-          num = 0;
-          flag = 0;
-        }
-        data[i] = num;
-        //printf("%d. num = %d\n",i,num);
-      }
-      c.a.a7 = data[0];
-      c.a.a6 = data[1];
-      c.a.a5 = data[2];
-      c.a.a4 = data[3];
-      c.a.a3 = data[4];
-      c.a.a2 = data[5];
-      c.a.a1 = data[6];
-      c.a.a0 = data[7];
-      chara = c.c;
-      printf("char is [%c]\n", chara);
-      str[k] = chara;
-      k++;
-      delay(TIME*2);
-    }
-    n++;
-  }
-  str[k-1] = '\0';
-  printf("str is [%s]\n", str);
+  pinMode(SS_PORT, OUTPUT);
+  digitalWrite(SS_PORT, 1);
   return 0;
+}
+
+void main(void)
+{
+  if (init_spi_channel() < 0) {
+    fprintf(stderr, "SPI channel initialization failed\n");
+    exit(-1);
+  }
+
+  while (1) {
+    int intensity = get_light_intensity();
+    printf("%d\n", intensity);
+
+    if (intensity >= THRESHOLD) {
+      printf("Data input.\n");
+      delay(SHORT_DELAY);
+
+      unsigned char data = 0;
+
+      // receive 8 bits.
+      for (int i = 0; i < 8; i++) {
+        data >>= 1;
+        int num = 0;
+
+        // take intensity three times.
+        for (int j = 0; j < 3; j++) {
+          int intensity = get_light_intensity();
+          if (intensity >= THRESHOLD) num++;
+          delay(SHORT_DELAY);
+        }
+
+        if (num >= 2) {
+          // set MSB
+          data &= 0x80;
+        }
+      }
+      printf("data is [%c]\n", data);
+      delay(SHORT_DELAY * 2);
+    }
+  }
 }
