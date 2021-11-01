9e72f39866d7027be0b4724f5ad215e3a7e0a71c
Yoshinari Nomura <nom@nomlab.org>
2021-11-01 16:45:46 +0900

    Remove extra spaces and tabs

--- src/recieve_str.c
+++ src/recieve_str.c
@@ -57,15 +57,15 @@ int main(void)
   int flag = 0;
   int try_num = 1000;
   //バッファ初期化
-  
+
   //SPIチャンネル初期化
   if((wiringPiSPISetup (SPI_CHANNEL, speed)) < 0){
     printf("wiringPiSPISetup error \n");
     return -1 ;
   }
-  
+
   printf("Setup SPI...\n");
-  
+
   //GPIO初期化
   if(wiringPiSetupGpio() == -1){
     printf("wiringPiSetupGpio error\n");
@@ -73,44 +73,44 @@ int main(void)
   }
   pinMode(SS_PORT, OUTPUT);                       //22pinを出力に設定
   digitalWrite(SS_PORT, 1);                       //SS信号初期化
-  
+
   printf("Start SPI...\n");
   while(n < try_num && flag == 0){
-    
+
     //SPI通信実行
     digitalWrite(SS_PORT, 0);                   //SS信号をLOW出力にして通信開始
     spi_buff[0] = 0b01100000;
     wiringPiSPIDataRW(SPI_CHANNEL, spi_buff, 2);             //データ送受信
     digitalWrite(SS_PORT, 1);                   //SS信号をHIGH出力にして通信終了
-    
+
     //インターバル
     printf("%d\n", spi_buff[0]*256+spi_buff[1]);                 //受信データを出力
-    
+
     if(check_v(spi_buff)){
       printf("Data input.\n");
       delay(TIME);
       flag = 1;
       for(i=0; i<8; i++){
-	num=0;
-	for(j=0; j<3; j++){
-	  digitalWrite(SS_PORT,0);
-	  spi_buff[0] = 0b01100000;
-	  wiringPiSPIDataRW(SPI_CHANNEL, spi_buff,2);
-	  digitalWrite(SS_PORT,1);
-	  spi_buff[0] = spi_buff[0] << 6;
-	  spi_buff[0] = spi_buff[0] >> 6;
-	  spi_buff[1] &= 0b11111111;
-	  //printf("\t%d\n", spi_buff[0]*256+spi_buff[1]);     //受信データを出力
-	  if(check_v(spi_buff))num += 1;
-	  delay(TIME);
-	}
-	if (num >= 2) num= 1;
-	else {
-	  num = 0;
-	  flag = 0;
-	}
-	data[i] = num;
-	//printf("%d. num = %d\n",i,num);
+        num=0;
+        for(j=0; j<3; j++){
+          digitalWrite(SS_PORT,0);
+          spi_buff[0] = 0b01100000;
+          wiringPiSPIDataRW(SPI_CHANNEL, spi_buff,2);
+          digitalWrite(SS_PORT,1);
+          spi_buff[0] = spi_buff[0] << 6;
+          spi_buff[0] = spi_buff[0] >> 6;
+          spi_buff[1] &= 0b11111111;
+          //printf("\t%d\n", spi_buff[0]*256+spi_buff[1]);     //受信データを出力
+          if(check_v(spi_buff))num += 1;
+          delay(TIME);
+        }
+        if (num >= 2) num= 1;
+        else {
+          num = 0;
+          flag = 0;
+        }
+        data[i] = num;
+        //printf("%d. num = %d\n",i,num);
       }
       c.a.a7 = data[0];
       c.a.a6 = data[1];
