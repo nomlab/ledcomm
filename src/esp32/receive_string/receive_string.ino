616e752fbe3f0f5b0bc03a1f11597c215401da95
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2021-11-17 11:12:54 +0900

    Update esp32 program

--- src/esp32/receive_string/receive_string.ino
+++ src/esp32/receive_string/receive_string.ino
@@ -1,9 +1,10 @@
 #include <WiFi.h>
 #include <unistd.h>
 
-#define THRESHOLD 300
 #define SHORT_DELAY 5
 #define LED_PIN 32
+
+int THRESHOLD = 0;
 
 void setup()
 {
@@ -14,8 +15,11 @@ void setup()
 void loop()
 {
   char str[64];
+  printf("---start---\n");
+  // print_v();
   LED_read_binaly(str);
   printf("str = [%s]\n", str);
+  // LED_on();
   delay(10000);
 }
 
@@ -33,7 +37,7 @@ void LED_off()
 
 int get_light_intensity()
 {
-  int intensity;
+  int intensity = 0;
   pinMode(LED_PIN, INPUT);
   digitalWrite(LED_PIN, 1);
   intensity = analogRead(LED_PIN);
@@ -60,67 +64,98 @@ void LED_read_binaly(char str_data[64])
   int n = 0;
   int light_count = 0;
   int timestamp = 0;
-  int binaly_count = 0;
-  int sig_len, sig_unit;
+  int sig_len = 0, sig_unit = 0;
   int edge_up_time, edge_down_time;
-  int bin_data[8] = {0};
   int bin_data_num = 0;
   int str_data_num = 0;
+  unsigned char data = 0;
 
   pinMode(LED_PIN, INPUT);
 
+  while (n < 100)
+  {
+    value = get_light_intensity();
+    if (THRESHOLD < value)
+    {
+      THRESHOLD = value;
+    }
+    n++;
+    delay(1);
+  }
+  printf("THRESHOLD : %d\n", THRESHOLD);
+
   while (1)
   {
-    if (n == 10000)
-    { //dodge a watchdog
-      delay(0);
-      n = 0;
+    if ((timestamp % 10000) == 0)
+    {
+      delay(0); // dodge a watchdog
     }
 
+    // blink LED and read intensity
+    LED_on();
+    delay(0);
+    LED_off();
     value = get_light_intensity() - THRESHOLD;
 
     if (value < 0)
     {
       value = 0;
-      n++;
     }
     else
     {
       light_count += 1;
     }
 
-    if (0 < binaly_count && sig_unit * 3.5 < light_count)
+    if ((sig_unit != 0) && (sig_unit * 3.5 < light_count))
+    {
+      printf("sig_unit : %d\n", sig_unit);
+      printf("light_count : %d\n", light_count);
       break;
+    }
 
     if (prev_v == 0 && value != 0)
     { // If edge up
-      edge_up_time = timestamp;
-      light_count = 0;
+      if ((sig_unit * 0.8) <= (timestamp - edge_down_time))
+      {
+        edge_up_time = timestamp;
+      }
     }
     else if (prev_v != 0 && value == 0)
-    { // If edge down
-      if (binaly_count == 0)
-      { // If signal_unit
-        if (10 < light_count)
+    {
+      // If edge down
+      edge_down_time = timestamp;
+      sig_len = edge_down_time - edge_up_time;
+      if (sig_unit == 0)
+      {
+        // If signal_unit comes
+        if (10 < sig_len)
         {
-          sig_unit = light_count;
-          binaly_count++;
+          printf("signal received\n");
+          sig_unit = sig_len;
           light_count = 0;
+          printf("sig_unit : %d\n", sig_unit);
+          printf("1 :");
         }
       }
       else
       {
-        edge_down_time = timestamp;
-        sig_len = edge_down_time - edge_up_time;
         if (sig_unit / 3 < sig_len && str_data_num < 64)
         {
-          bin_data[bin_data_num] = signal_check((double)sig_unit, (double)sig_len);
+          printf(" %d ", sig_len); // print sig_len
+          if (sig_unit < sig_len * 0.8)
+          {
+            data |= 0x80;
+          }
+          data >>= 1;
           bin_data_num++;
           if (bin_data_num == 8)
           {
-            // str_data[str_data_num] = char_converter(bin_data);
+            printf("\n");
+            printf("char : %c\n", data);
+            str_data[str_data_num] = data;
             str_data_num++;
             bin_data_num = 0;
+            printf("%d :", str_data_num + 1);
           }
         }
         light_count = 0;
@@ -130,16 +165,4 @@ void LED_read_binaly(char str_data[64])
     timestamp++;
   }
   str_data[str_data_num] = '\0';
-}
-
-int signal_check(double signal_unit, double signal_len)
-{
-  if ((signal_len * 0.9 < signal_unit))
-  {
-    return 0;
-  }
-  else
-  {
-    return 1;
-  }
 }
--- src/esp32/receive_wifi_info/receive_wifi_info.ino
+++ src/esp32/receive_wifi_info/receive_wifi_info.ino
@@ -5,49 +5,55 @@
 #define SHORT_DELAY 5
 #define LED_PIN 32
 
-void setup() {
+void setup()
+{
   // initialize both serial ports:
   Serial.begin(115200);
-
 }
 
-void loop() {
+void loop()
+{
   //print_v();
   wifi_setup();
   delay(10000);
 }
 
-void LED_on() {
+void LED_on()
+{
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 1);
 }
 
-void LED_off() {
+void LED_off()
+{
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 0);
 }
 
 int get_light_intensity()
 {
-  int instensity;
+  int intensity;
   pinMode(LED_PIN, INPUT);
   digitalWrite(LED_PIN, 1);
   intensity = analogRead(LED_PIN);
   digitalWrite(LED_PIN, 0);
-  return instensity;
+  return intensity;
 }
 
-void print_v() {
+void print_v()
+{
   int value;
   pinMode(LED_PIN, INPUT);
   value = analogRead(LED_PIN);
   printf("v=%d", value);
-  if (THRESHOLD < value)printf("<----  t");
+  if (THRESHOLD < value)
+    printf("<----  t");
   printf("\n");
   delay(95);
 }
 
-void wifi_setup() {
+void wifi_setup()
+{
   int i, n = 0;
   int v;
   char ssid[64];
@@ -56,45 +62,48 @@ void wifi_setup() {
   printf("---Start VLC---\n");
 
   printf("Waiting for data...\n");
-  while (true) {
-      LED_on();
-      usleep(SHORT_DELAY);
-      LED_off();
-      usleep(SHORT_DELAY);
-    if (get_light_intensity() >= THRESHOLD)break;
+  while (true)
+  {
+    LED_on();
+    usleep(SHORT_DELAY);
+    LED_off();
+    usleep(SHORT_DELAY);
+    if (get_light_intensity() >= THRESHOLD)
+      break;
   }
   printf("SSID recieved.\n");
   LED_read_binaly(ssid);
   printf("SSID : ");
-  else printf("%s\n", ssid);
+  printf("%s\n", ssid);
   delay(100);
 
   //printf("Waiting for data...\n");
   LED_read_binaly(passwd);
   printf("PASS recieved.\n");
   printf("PASS : ");
-  printf("%s\n","**********");
+  printf("%s\n", "**********");
 
   wifi_status = connectWiFi(ssid, passwd);
-  switch (wifi_status) {
-    case 0: //Success
-      pinMode(LED_PIN, OUTPUT);
+  switch (wifi_status)
+  {
+  case 0: //Success
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, 1);
+    break;
+  case 1: //Failed
+    pinMode(LED_PIN, OUTPUT);
+    for (i = 0; i < 10; i++)
+    {
       digitalWrite(LED_PIN, 1);
-      break;
-    case 1: //Failed
-      pinMode(LED_PIN, OUTPUT);
-      for (i = 0; i < 10; i++) {
-        digitalWrite(LED_PIN, 1);
-        delay(250);
-        digitalWrite(LED_PIN, 0);
-        delay(250);
-      }
-      break;
-    default:
-      break;
+      delay(250);
+      digitalWrite(LED_PIN, 0);
+      delay(250);
+    }
+    break;
+  default:
+    break;
   }
 }
-
 
 int connectWiFi(char *ssid, char *passwd)
 {
@@ -104,11 +113,13 @@ int connectWiFi(char *ssid, char *passwd)
 
   WiFi.begin(ssid, passwd);
   printf("Connecting to %s\n", "DUMMY-SSID-2");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     //printf("%d Status : %d\n", try_times, WiFi.status());
     Serial.print(".");
     delay(500);
-    if (30 < try_times++) {
+    if (30 < try_times++)
+    {
       printf("\nWiFi connection failed.\n");
       return 1;
     }
@@ -117,7 +128,8 @@ int connectWiFi(char *ssid, char *passwd)
   return 0;
 }
 
-void LED_read_binaly(char str_data[64]) {
+void LED_read_binaly(char str_data[64])
+{
   int value;
   int prev_v = 0;
   int n = 0;
@@ -132,44 +144,56 @@ void LED_read_binaly(char str_data[64]) {
 
   pinMode(LED_PIN, INPUT);
 
-  while (1) {
-    if (n == 10000) { //dodge a watchdog
+  while (1)
+  {
+    if (n == 10000)
+    { //dodge a watchdog
       delay(0);
       n = 0;
     }
 
     value = get_light_intensity() - THRESHOLD;
 
-    if (value < 0) {
+    if (value < 0)
+    {
       value = 0;
       n++;
     }
-    else {
+    else
+    {
       light_count += 1;
     }
 
-    if (0 < binaly_count && sig_unit * 3.5 < light_count)break;
+    if (0 < binaly_count && sig_unit * 3.5 < light_count)
+      break;
 
-    if (prev_v == 0 && value != 0) { // If edge up
+    if (prev_v == 0 && value != 0)
+    { // If edge up
       edge_up_time = timestamp;
       light_count = 0;
     }
-    else if (prev_v != 0 && value == 0) {// If edge down
-      if (binaly_count == 0) {// If signal_unit
-        if (10 < light_count) {
+    else if (prev_v != 0 && value == 0)
+    { // If edge down
+      if (binaly_count == 0)
+      { // If signal_unit
+        if (10 < light_count)
+        {
           sig_unit = light_count;
           binaly_count++;
           light_count = 0;
         }
       }
-      else {
+      else
+      {
         edge_down_time = timestamp;
         sig_len = edge_down_time - edge_up_time;
-        if (sig_unit / 3 < sig_len  && str_data_num < 64) {
+        if (sig_unit / 3 < sig_len && str_data_num < 64)
+        {
           bin_data[bin_data_num] = signal_check((double)sig_unit, (double)sig_len);
           bin_data_num++;
-          if (bin_data_num == 8) {
-            str_data[str_data_num] = char_converter(bin_data);
+          if (bin_data_num == 8)
+          {
+            str_data[str_data_num] = 0;
             str_data_num++;
             bin_data_num = 0;
           }
@@ -183,10 +207,14 @@ void LED_read_binaly(char str_data[64]) {
   str_data[str_data_num] = '\0';
 }
 
-int signal_check(double signal_unit, double signal_len) {
-  if (( signal_len * 0.9 < signal_unit) ) {
+int signal_check(double signal_unit, double signal_len)
+{
+  if ((signal_len * 0.9 < signal_unit))
+  {
     return 0;
-  }else {
+  }
+  else
+  {
     return 1;
   }
 }
