add24cff195251c9307d0fb3b28014e9ae909e5f
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2022-01-05 10:35:15 +0900

    Update esp32 scripts

--- src/esp32/receive_string/receive_string.ino
+++ src/esp32/receive_string/receive_string.ino
@@ -8,35 +8,33 @@
 #define LED_PIN 32
 SSD1306 display(0x3c, 21, 22, GEOMETRY_128_32);
 
-int THRESHOLD = 200;
-
-int data[1000000];
+int THRESHOLD = 100;
 
 void setup()
 {
   // initialize both serial ports:
-  init_display();
+  draw_display("Hello.");
   Serial.begin(115200);
 }
 
 void loop()
 {
   char str[64] = "\0";
+  // char str_display[100] = "\0";
   // printf("---start---\n");
   LED_read_binaly(str);
   printf("str = [%s]\n", str);
-  display.clear();
-  display.drawString(0, 0, str); //(0,0)の位置にstrを表示
-  display.display();
-  delay(5000);
+  // sprintf(str_display, "Data : [%s]\n", str);
+  draw_display(str);
+  delay(15 * 1000);
   // print_v();
 }
 
-void init_display()
+void draw_display(char *str)
 {
-  display.init();                           //ディスプレイを初期化
-  display.setFont(ArialMT_Plain_16);        //フォントを設定
-  display.drawString(0, 0, "0123456789as"); //(0,0)の位置にHello Worldを表示
+  display.init();                    //ディスプレイを初期化
+  display.setFont(ArialMT_Plain_16); //フォントを設定
+  display.drawString(0, 0, str);     //(0,0)の位置にHello Worldを表示
   display.display();
 }
 
@@ -89,7 +87,10 @@ void LED_read_binaly(char str_data[64])
   int value;
   int prev_v = 0;
   int n = 0;
+  int blink_flag = 1;
   __int64_t timestamp;
+  __int64_t blink_next_time;
+  __int64_t blink_time_interval = 700 * 1000;
   __int64_t sig_len = 0, sig_unit = 0;
   __int64_t edge_up_time = 0, edge_down_time = 0;
   int bin_data_num = 0;
@@ -111,13 +112,31 @@ void LED_read_binaly(char str_data[64])
   THRESHOLD = 200;
   // printf("THRESHOLD : %d\n", THRESHOLD);
 
+  draw_display("Ready to read.");
+
+  blink_next_time = esp_timer_get_time() + blink_time_interval;
+
   while (1)
   {
     // blink LED and read intensity
     timestamp = esp_timer_get_time();
-    LED_on();
-    delay(0);
-    LED_off();
+    if (blink_next_time < timestamp)
+    {
+      blink_flag ^= 1;
+      blink_next_time = timestamp + blink_time_interval;
+      printf(" %" PRId64 "\n", timestamp);       // print timestamp
+      printf(" %" PRId64 "\n", blink_next_time); // print blink_next_time
+    }
+
+    if (blink_flag == 1)
+    {
+      LED_on();
+    }
+    else
+    {
+      LED_off();
+    }
+
     value = get_light_intensity() - THRESHOLD;
 
     if (value < 0)
@@ -132,7 +151,7 @@ void LED_read_binaly(char str_data[64])
     }
     if (sig_unit != 0)
     {
-      printf("%" PRId64 ",%d\n", timestamp, value);
+      // printf("%" PRId64 ",%d\n", timestamp, value);
     }
 
     if (prev_v == 0 && value != 0)
@@ -153,7 +172,8 @@ void LED_read_binaly(char str_data[64])
       if (sig_unit == 0)
       {
         // If signal_unit comes
-        printf("signal received\n");
+        printf("signal recieved\n");
+        draw_display("Signal recieved.");
         sig_unit = sig_len;
         // printf("sig_unit : %" PRId64 "\n", sig_unit);
         // printf("1 :");
@@ -162,21 +182,26 @@ void LED_read_binaly(char str_data[64])
       {
         if (sig_unit / 3 < sig_len && str_data_num < 64)
         {
-          // printf(" %" PRId64 "", sig_len); // print sig_len
+          printf(" %" PRId64 "\n", sig_len); // print sig_len
           if (sig_unit < sig_len * 0.8)
           {
-            data |= 0x80;
+            data |= 0x01;
           }
-          data >>= 1;
-          bin_data_num++;
-          if (bin_data_num == 8)
+
+          if (bin_data_num == 7)
           {
             // printf("\n");
-            // printf("char : %c\n", data);
+            printf("char : %c\n", data);
             str_data[str_data_num] = data;
             str_data_num++;
             bin_data_num = 0;
+            data = 0;
             // printf("%d :", str_data_num + 1);
+          }
+          else
+          {
+            data <<= 1;
+            bin_data_num++;
           }
         }
       }
--- src/esp32/receive_wifi_info/receive_wifi_info.ino
+++ src/esp32/receive_wifi_info/receive_wifi_info.ino
@@ -4,10 +4,13 @@
 #include <inttypes.h>
 #include "esp_system.h"
 #include "esp_timer.h"
+#include "SSD1306.h"
 
 #define LED_PIN 32
+#define SHORT_DELAY 500
+SSD1306 display(0x3c, 21, 22, GEOMETRY_128_32);
 
-int THRESHOLD = 0;
+int THRESHOLD = 200;
 
 void setup()
 {
@@ -17,7 +20,6 @@ void setup()
 
 void loop()
 {
-  //print_v();
   wifi_setup();
   delay(10000);
 }
@@ -36,24 +38,20 @@ void LED_off()
 
 int get_light_intensity()
 {
-  int intensity;
-  pinMode(LED_PIN, INPUT);
-  digitalWrite(LED_PIN, 1);
-  intensity = analogRead(LED_PIN);
+  int intensity = 0;
+  pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, 0);
+  pinMode(LED_PIN, ANALOG);
+  intensity = analogRead(LED_PIN);
   return intensity;
 }
 
-void print_v()
+void draw_display(char *str)
 {
-  int value;
-  pinMode(LED_PIN, INPUT);
-  value = analogRead(LED_PIN);
-  printf("v=%d", value);
-  if (THRESHOLD < value)
-    printf("<----  t");
-  printf("\n");
-  delay(95);
+  display.init();                    //ディスプレイを初期化
+  display.setFont(ArialMT_Plain_16); //フォントを設定
+  display.drawString(0, 0, str);     //(0,0)の位置にHello Worldを表示
+  display.display();
 }
 
 void wifi_setup()
@@ -66,22 +64,12 @@ void wifi_setup()
   printf("---Start VLC---\n");
 
   printf("Waiting for data...\n");
-  while (true)
-  {
-    LED_on();
-    usleep(SHORT_DELAY);
-    LED_off();
-    usleep(SHORT_DELAY);
-    if (get_light_intensity() >= THRESHOLD)
-      break;
-  }
-  printf("SSID recieved.\n");
   LED_read_binaly(ssid);
   printf("SSID : ");
   printf("%s\n", ssid);
   delay(100);
 
-  //printf("Waiting for data...\n");
+  // printf("Waiting for data...\n");
   LED_read_binaly(passwd);
   printf("PASS recieved.\n");
   printf("PASS : ");
@@ -90,11 +78,11 @@ void wifi_setup()
   wifi_status = connectWiFi(ssid, passwd);
   switch (wifi_status)
   {
-  case 0: //Success
+  case 0: // Success
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, 1);
     break;
-  case 1: //Failed
+  case 1: // Failed
     pinMode(LED_PIN, OUTPUT);
     for (i = 0; i < 10; i++)
     {
@@ -117,7 +105,7 @@ int connectWiFi(char *ssid, char *passwd)
   printf("Connecting to %s\n", "DUMMY-SSID-2");
   while (WiFi.status() != WL_CONNECTED)
   {
-    //printf("%d Status : %d\n", try_times, WiFi.status());
+    // printf("%d Status : %d\n", try_times, WiFi.status());
     Serial.print(".");
     delay(500);
     if (30 < try_times++)
@@ -135,7 +123,10 @@ void LED_read_binaly(char str_data[64])
   int value;
   int prev_v = 0;
   int n = 0;
+  int blink_flag = 1;
   __int64_t timestamp;
+  __int64_t blink_next_time;
+  __int64_t blink_time_interval = 700 * 1000;
   __int64_t sig_len = 0, sig_unit = 0;
   __int64_t edge_up_time = 0, edge_down_time = 0;
   int bin_data_num = 0;
@@ -154,15 +145,34 @@ void LED_read_binaly(char str_data[64])
     n++;
     delay(1);
   }
-  printf("THRESHOLD : %d\n", THRESHOLD);
+  THRESHOLD = 200;
+  // printf("THRESHOLD : %d\n", THRESHOLD);
+
+  draw_display("Ready to read.");
+
+  blink_next_time = esp_timer_get_time() + blink_time_interval;
 
   while (1)
   {
     // blink LED and read intensity
     timestamp = esp_timer_get_time();
-    LED_on();
-    delay(0);
-    LED_off();
+    if (blink_next_time < timestamp)
+    {
+      blink_flag ^= 1;
+      blink_next_time = timestamp + blink_time_interval;
+      // printf(" %" PRId64 "\n", timestamp);       // print timestamp
+      // printf(" %" PRId64 "\n", blink_next_time); // print blink_next_time
+    }
+
+    if (blink_flag == 1)
+    {
+      LED_on();
+    }
+    else
+    {
+      LED_off();
+    }
+
     value = get_light_intensity() - THRESHOLD;
 
     if (value < 0)
@@ -172,13 +182,17 @@ void LED_read_binaly(char str_data[64])
 
     if ((sig_unit != 0) && (sig_unit * 4 < (timestamp - edge_up_time)) && (edge_down_time < edge_up_time))
     {
-      printf("\nsig_unit : %" PRId64 "\n", sig_unit);
+      // printf("\nsig_unit : %" PRId64 "\n", sig_unit);
       break;
+    }
+    if (sig_unit != 0)
+    {
+      // printf("%" PRId64 ",%d\n", timestamp, value);
     }
 
     if (prev_v == 0 && value != 0)
     { // If edge up
-      if ((sig_unit * 0.8) <= (timestamp - edge_down_time))
+      if ((sig_unit * 0.5) <= (timestamp - edge_down_time))
       {
         edge_up_time = timestamp;
         // printf("(in edge up) edge_up_time : %g\n", edge_up_time);
@@ -194,30 +208,36 @@ void LED_read_binaly(char str_data[64])
       if (sig_unit == 0)
       {
         // If signal_unit comes
-        printf("signal received\n");
+        printf("signal recieved\n");
+        draw_display("Signal recieved.");
         sig_unit = sig_len;
-        printf("sig_unit : %" PRId64 "\n", sig_unit);
-        printf("1 :");
+        // printf("sig_unit : %" PRId64 "\n", sig_unit);
+        // printf("1 :");
       }
       else
       {
         if (sig_unit / 3 < sig_len && str_data_num < 64)
         {
-          printf(" %" PRId64 "", sig_len); // print sig_len
+          printf(" %" PRId64 "\n", sig_len); // print sig_len
           if (sig_unit < sig_len * 0.8)
           {
-            data |= 0x80;
+            data |= 0x01;
           }
-          data >>= 1;
-          bin_data_num++;
-          if (bin_data_num == 8)
+
+          if (bin_data_num == 7)
           {
-            printf("\n");
+            // printf("\n");
             printf("char : %c\n", data);
             str_data[str_data_num] = data;
             str_data_num++;
             bin_data_num = 0;
-            printf("%d :", str_data_num + 1);
+            data = 0;
+            // printf("%d :", str_data_num + 1);
+          }
+          else
+          {
+            data <<= 1;
+            bin_data_num++;
           }
         }
       }
