6f929b9834821b4f4ade278c4f8250224453ba6d
hamamoto-j <p5eq792y@s.okayama-u.ac.jp>
2022-01-06 09:49:57 +0900

    Imprement new VLC protocol to esp32 program

--- src/esp32/receive_string/receive_string.ino
+++ src/esp32/receive_string/receive_string.ino
@@ -8,7 +8,7 @@
 #define LED_PIN 32
 SSD1306 display(0x3c, 21, 22, GEOMETRY_128_32);
 
-int THRESHOLD = 100;
+int THRESHOLD = 0;
 
 void setup()
 {
@@ -84,14 +84,17 @@ void print_v()
 
 void LED_read_binaly(char str_data[64])
 {
-  int value;
-  int prev_v = 0;
+  int value = 0, prev_v = 0;
+  int value_bright = 0, value_dark = 0;
+  int value_bright_num = 0, value_dark_num = 0;
   int n = 0;
   int blink_flag = 1;
   __int64_t timestamp;
+  __int64_t prev_timestamp;
   __int64_t blink_next_time;
   __int64_t blink_time_interval = 700 * 1000;
   __int64_t sig_len = 0, sig_unit = 0;
+  __int64_t sig_reader_bright = 0, sig_reader_dark = 0;
   __int64_t edge_up_time = 0, edge_down_time = 0;
   int bin_data_num = 0;
   int str_data_num = 0;
@@ -99,7 +102,9 @@ void LED_read_binaly(char str_data[64])
 
   pinMode(LED_PIN, INPUT);
 
-  while (n < 100)
+  THRESHOLD = 0;
+
+  while (n < 1000)
   {
     value = get_light_intensity();
     if (THRESHOLD < value)
@@ -109,23 +114,26 @@ void LED_read_binaly(char str_data[64])
     n++;
     delay(1);
   }
-  THRESHOLD = 200;
-  // printf("THRESHOLD : %d\n", THRESHOLD);
+  THRESHOLD += 1;
+  printf("THRESHOLD 1 : %d\n", THRESHOLD);
 
   draw_display("Ready to read.");
 
+  timestamp = esp_timer_get_time();
+  prev_timestamp = timestamp;
   blink_next_time = esp_timer_get_time() + blink_time_interval;
 
-  while (1)
+  while (value <= THRESHOLD)
   {
+    value = get_light_intensity();
     // blink LED and read intensity
     timestamp = esp_timer_get_time();
     if (blink_next_time < timestamp)
     {
       blink_flag ^= 1;
       blink_next_time = timestamp + blink_time_interval;
-      printf(" %" PRId64 "\n", timestamp);       // print timestamp
-      printf(" %" PRId64 "\n", blink_next_time); // print blink_next_time
+      // printf(" %" PRId64 "\n", timestamp);       // print timestamp
+      // printf(" %" PRId64 "\n", blink_next_time); // print blink_next_time
     }
 
     if (blink_flag == 1)
@@ -136,27 +144,82 @@ void LED_read_binaly(char str_data[64])
     {
       LED_off();
     }
+  }
+  LED_off();
+
+  printf("in light 1\n");
+
+  while (value <= ((THRESHOLD + 1) * 3))
+  {
+    value = get_light_intensity();
+  }
+
+  printf("in light 2\n");
+  // リーダの取得
+  while (1)
+  {
+    value = get_light_intensity();
+    timestamp = esp_timer_get_time();
+    prev_timestamp = timestamp;
+    while ((THRESHOLD * 3) < value)
+    {
+      value_bright_num++;
+      value_bright += value;
+      timestamp = esp_timer_get_time();
+      sig_reader_bright += (timestamp - prev_timestamp);
+      prev_timestamp = timestamp;
+      value = get_light_intensity();
+    }
+    printf("bright %" PRId64 "\n", sig_reader_bright);
+
+    timestamp = esp_timer_get_time();
+    prev_timestamp = timestamp;
+    while (value <= (THRESHOLD * 3))
+    {
+      value_dark_num++;
+      value_dark += value;
+      timestamp = esp_timer_get_time();
+      sig_reader_dark += (timestamp - prev_timestamp);
+      prev_timestamp = timestamp;
+      value = get_light_intensity();
+
+      // printf("in dark\n");
+    }
+    printf("dark %" PRId64 "\n", sig_reader_dark);
+
+    if (sig_reader_bright / 2 <= sig_reader_dark)
+    {
+      THRESHOLD = ((value_bright / value_bright_num) + (value_dark / value_dark_num)) / 3;
+      break;
+    }
+  }
+  printf("THRESHOLD 2 : %d\n", THRESHOLD);
+
+  sig_unit = sig_reader_bright / 8;
+  printf("sig_unit : %" PRId64 "\n", sig_unit);
+
+  value = 0;
+  prev_v = 0;
+
+  // データの取得
+  while (1)
+  {
+    timestamp = esp_timer_get_time();
+    if ((sig_unit * 5 < (timestamp - edge_up_time)) && (edge_down_time < edge_up_time))
+    {
+      // printf("\nsig_unit : %" PRId64 "\n", sig_unit);
+      break;
+    }
 
     value = get_light_intensity() - THRESHOLD;
-
     if (value < 0)
     {
       value = 0;
     }
 
-    if ((sig_unit != 0) && (sig_unit * 4 < (timestamp - edge_up_time)) && (edge_down_time < edge_up_time))
-    {
-      // printf("\nsig_unit : %" PRId64 "\n", sig_unit);
-      break;
-    }
-    if (sig_unit != 0)
-    {
-      // printf("%" PRId64 ",%d\n", timestamp, value);
-    }
-
     if (prev_v == 0 && value != 0)
     { // If edge up
-      if ((sig_unit * 0.8) <= (timestamp - edge_down_time))
+      if ((sig_unit * 0.7) <= (timestamp - edge_down_time))
       {
         edge_up_time = timestamp;
         // printf("(in edge up) edge_up_time : %g\n", edge_up_time);
@@ -169,40 +232,28 @@ void LED_read_binaly(char str_data[64])
       // printf("edge_up_time : %g\n", edge_up_time);
       // printf("edge_down_time : %g\n", edge_down_time);
       sig_len = edge_down_time - edge_up_time;
-      if (sig_unit == 0)
+      if (sig_unit / 2 < sig_len && str_data_num < 64)
       {
-        // If signal_unit comes
-        printf("signal recieved\n");
-        draw_display("Signal recieved.");
-        sig_unit = sig_len;
-        // printf("sig_unit : %" PRId64 "\n", sig_unit);
-        // printf("1 :");
-      }
-      else
-      {
-        if (sig_unit / 3 < sig_len && str_data_num < 64)
+        printf(" %" PRId64 "\n", sig_len); // print sig_len
+        if (sig_unit < sig_len * 0.6)
         {
-          printf(" %" PRId64 "\n", sig_len); // print sig_len
-          if (sig_unit < sig_len * 0.8)
-          {
-            data |= 0x01;
-          }
+          data |= 0x01;
+        }
 
-          if (bin_data_num == 7)
-          {
-            // printf("\n");
-            printf("char : %c\n", data);
-            str_data[str_data_num] = data;
-            str_data_num++;
-            bin_data_num = 0;
-            data = 0;
-            // printf("%d :", str_data_num + 1);
-          }
-          else
-          {
-            data <<= 1;
-            bin_data_num++;
-          }
+        if (bin_data_num == 7)
+        {
+          // printf("\n");
+          printf("char : %c\n", data);
+          str_data[str_data_num] = data;
+          str_data_num++;
+          bin_data_num = 0;
+          data = 0;
+          // printf("%d :", str_data_num + 1);
+        }
+        else
+        {
+          data <<= 1;
+          bin_data_num++;
         }
       }
     }
